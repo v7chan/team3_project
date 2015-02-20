3b4f8573b7955e4a9d170507ef9e7fa05de036c7
Vincent Chan <v7chan@gmail.com>
2015-02-19 16:01:38 -0800

    Check if user object exists before rendering search page

--- routes/index.js
+++ routes/index.js
@@ -2,20 +2,22 @@ var express = require('express');
 var router = express.Router();
 var searchFields = require("../helpers/search_fields.json");
 
+var updatedSearchFields = []
+updatedSearchFields.push(searchFields[0]);
+updatedSearchFields.push(searchFields[1]);
+updatedSearchFields.push(searchFields[2]);
+updatedSearchFields.push(searchFields[3]);
+
 /* GET home page. */
 router.get('/', function(req, res) {
   res.render('splash', { title: "Team3 | TritonEATS!", user: req.user });
 });
 
 router.get('/search_page', function(req, res) {
-  var updatedSearchFields = []
-  updatedSearchFields.push(searchFields[0]);
-  updatedSearchFields.push(searchFields[1]);
-  updatedSearchFields.push(searchFields[2]);
-  updatedSearchFields.push(searchFields[3]);
-
-  updatedSearchFields[2].placeholder = req.user.goal;
-  updatedSearchFields[3].placeholder = req.user.restriction;
+  if(typeof req.user != 'undefined') {
+    updatedSearchFields[2].placeholder = req.user.goal;
+    updatedSearchFields[3].placeholder = req.user.restriction;
+  }
 
   res.render('index', { title: "Team3 | TritonEATS!", 'fields': updatedSearchFields, user: req.user });
 });
