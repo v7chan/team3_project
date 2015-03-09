5bd285585815f46dbb62ee56ba20d14a902b215a
Jennifer Lu <jenniferlu39@gmail.com>
2015-03-08 17:11:30 -0700

    fixed saving filters that user chooses

--- public/javascripts/search.js
+++ public/javascripts/search.js
@@ -6,10 +6,14 @@ $(document).ready(function() {
 function listenForSearch() {
   $('#search-food').click(function(e) {
     e.preventDefault();
-    var location     = $('#select-locations-current').attr('placeholder');
+  /*  var location     = $('#select-locations-current').attr('placeholder');
     var time         = $('#select-time-current').attr('placeholder').split(" ")[0];
     var goal         = $('#select-goal-current').attr('placeholder');
-    var restrictions = $('#select-restrictions-current').attr('placeholder');
+    var restrictions = $('#select-restrictions-current').attr('placeholder'); */
+    var location     = $('#select-locations-current').val();
+    var time         = $('#select-time-current').val().split(" ")[0];
+    var goal         = $('#select-goal-current').val();
+    var restrictions = $('#select-restrictions-current').val();
 
     window.location.href = '/search/results?location=' + location + 
     '&time=' + time +
